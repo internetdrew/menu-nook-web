77efbcbeefbc9faa0508678f7f88eecff870e185
Andrew Rowley <andrew@internetdrew.com>
2026-08-27 09:39:10 -0400

    update to free

--- src/constants.ts
+++ src/constants.ts
@@ -36,7 +36,7 @@ export type MenuCategory = {
   items: MenuItem[];
 };
 
-export const CTA_COPY = "Put your store's menu online";
+export const CTA_COPY = "Get your menu online";
 
 export const categorizedItems: MenuCategory[] = [
   {
